73151537806858cea9535f5f5f461dc4930c38e6
Balavinayak R <vinayak1986@gmail.com>
2019-04-15 06:54:37 +0530

    Added SMS logic and code to retry POST if an error is thrown

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 The script is used to fetch the status of the CDR application submitted to Engineers Australia from their portal. We use the *requests* library to first execute a POST call to the site using the required parameters. This is followed by a GET call on the application home page to get the status.
 
+Apr 15, 2019
+Added logic to send SMS if the status changes from 'Queued for assessment'. Also, the POST call will now try to execute 5 times if an error code is thrown.
+
--- ea_check_status.py
+++ ea_check_status.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import html
+import re
 
 # Function to get application status from EA portal.
 def get_app_status(user_name, password, login_url, status_page):
@@ -11,7 +12,17 @@ def get_app_status(user_name, password, login_url, status_page):
 	site_token = list(tree.xpath("//input[@name = 'form_build_id']/@value"))[0]
 	# The params for the POST call were determined from 'Network Monitor' in Firefox.
 	payload = {'name' : user_name, 'pass' : password, 'op' : 'Login', 'form_build_id' : site_token, 'form_id' : 'user_login'}
-	session_request.post(login_url, data = payload, headers = dict(referer = login_url))
+	# Try the POST logic 5 times if an error code is returned.
+	found = False
+	n_tries = 1
+	while (not found) and (n_tries <= 5):
+		try:
+			response = session_request.post(login_url, data = payload, headers = dict(referer = login_url))
+			if response.status_code == '200':
+				found = True
+			n_tries += 1
+		except:
+			pass
 	# The referer in the GET call was earlier the 'status_page'. This returned wrong
 	# results and since has been replaced with 'login_url'.
 	result = session_request.get(status_page, headers = dict(referer = login_url))
@@ -20,6 +31,14 @@ def get_app_status(user_name, password, login_url, status_page):
 	status = list(tree.xpath("//table[@class='progress-table']/tbody/tr/td[3]/text()"))[0]
 	return status
 
+def send_sms():
+	url = "https://www.fast2sms.com/dev/bulk"
+	payload = "sender_id=FSTSMS&language=english&route=qt&numbers=9739679174,9480373931&message=8923"
+	headers = {'authorization': "Gs5QbmdjV9IEwKnXfBhRCPtL0paJ1Fviur7leA3ZzWU8HNTkq4SWupjCDAR74wTgtLX5Fl0qeUOkmodM",
+    		   'cache-control': "no-cache",
+    		   'content-type': "application/x-www-form-urlencoded"}
+	requests.request("POST", url, data=payload, headers=headers)
+
 if __name__ == '__main__':
 	ea_username = 'vinayak1986@gmail.com'
 	ea_pwd = 'H312@blr'
@@ -27,4 +46,7 @@ if __name__ == '__main__':
 	ea_status_page = 'https://portal.engineersaustralia.org.au/estage1/applicant'
 	status = get_app_status(ea_username, ea_pwd, ea_login_url, ea_status_page)
 	print(status)
+    # Send SMS if the status changes from 'Queued for assessment'
+	if (not re.match('Queued', status)) and status:
+		send_sms()
 
